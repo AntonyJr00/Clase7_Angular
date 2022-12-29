15a71b0e00f96681f55a5b13060ebd0dd846bc5f
AntonyJr00 <antonyrodl20@gmail.com>
2022-12-29 17:37:56 -0500

    nav y crew terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CrewModule } from './crew/crew.module';
+import { NavModule } from './nav/nav.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CrewModule, NavModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
