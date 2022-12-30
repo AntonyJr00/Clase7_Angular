e00ca0e5480b3cfe6103944c09a0b6014908477a
Dinelyp <dinely@rapihost.com>
2022-12-29 23:54:10 -0500

    Subiendo technology

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TechnologyModule } from './technology/technology.module';TechnologyModule
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TechnologyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
