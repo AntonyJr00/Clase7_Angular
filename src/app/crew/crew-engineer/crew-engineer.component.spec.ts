d0d8e797af2c0478bcee022b1c1d596a3e3b2f38
Estrella <108537918+star1602@users.noreply.github.com>
2022-12-29 23:39:01 -0500

    Subiendo cambios / Página Home

---
+++ src/app/crew/crew-engineer/crew-engineer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CrewEngineerComponent } from './crew-engineer.component';
+
+describe('CrewEngineerComponent', () => {
+  let component: CrewEngineerComponent;
+  let fixture: ComponentFixture<CrewEngineerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CrewEngineerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CrewEngineerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
