7a296f68a775cef5f675a66c0ada21c9b5270cd5
AntonyJr00 <antonyrodl20@gmail.com>
2022-12-30 12:42:47 -0500

    complete casi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CrewModule } from './crew/crew.module';
+import { HomeModule } from './home/home.module';
 import { NavModule } from './nav/nav.module';
+import { TechnologyModule } from './technology/technology.module';
+
+import { CrewEngineerComponent } from './crew/crew-engineer/crew-engineer.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { TechnologyInteriorComponent } from './technology/technology-interior/technology-interior.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CrewModule, NavModule],
+  imports: [
+    BrowserModule,
+    CrewModule,
+    NavModule,
+    HomeModule,
+    TechnologyModule,
+    RouterModule.forRoot([
+      { path: 'crew', component: CrewEngineerComponent },
+      { path: 'home', component: InicioComponent },
+      { path: 'tech', component: TechnologyInteriorComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { InicioComponent } from './inicio/inicio.component';
+import { NavModule } from '../nav/nav.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [InicioComponent],
+  imports: [CommonModule, NavModule],
+  exports: [InicioComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
