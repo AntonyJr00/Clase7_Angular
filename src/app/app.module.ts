f117482585ac0c3a564ad1f28ebaf16e9f64d1fe
AntonyJr00 <antonyrodl20@gmail.com>
2022-12-31 10:17:01 -0500

    trabajo terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CrewModule } from './crew/crew.module';
-import { HomeModule } from './home/home.module';
-import { NavModule } from './nav/nav.module';
-import { TechnologyModule } from './technology/technology.module';
-import { DestinationModule } from './destination/destination.module';
-import { SharedModule } from './destination/shared/shared.module';
-
-import { InicioComponent } from './home/inicio/inicio.component';
-import { CrewEngineerComponent } from './crew/crew-engineer/crew-engineer.component';
-import { TechnologyInteriorComponent } from './technology/technology-interior/technology-interior.component';
-import { HighlightComponent } from './destination/highlight/highlight.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    SharedModule,
-    DestinationModule,
     BrowserModule,
-    CrewModule,
-    NavModule,
-    HomeModule,
-    TechnologyModule,
-    RouterModule.forRoot([
-      { path: 'crew', component: CrewEngineerComponent },
-      { path: '', component: InicioComponent },
-      { path: 'destino', component: HighlightComponent },
-      { path: 'tech', component: TechnologyInteriorComponent },
-    ]),
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/crew/crew-engineer/crew-engineer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-crew-engineer',
-  templateUrl: './crew-engineer.component.html',
-  styleUrls: ['./crew-engineer.component.css'],
-})
-export class CrewEngineerComponent {}
